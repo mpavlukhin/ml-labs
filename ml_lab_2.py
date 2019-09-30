dfcc2a412e7fcf2226ff7c1db07c674b916f9aff
Max Pavlukhin <pavlukhinm@gmail.com>
2019-09-30 16:06:11 +0300

    Add blobs points sample generator
    Add generate_blobs_points_sample function
    that generate blobs points sample
    
    Related: MLLABS-2

--- ml_lab_2.py
+++ ml_lab_2.py
@@ -1,17 +1,35 @@
 import argparse
 
+from sklearn.datasets.samples_generator import make_blobs
+
 
 def get_args_parser():
     parser = argparse.ArgumentParser(
         description='ML Lab 2'
     )
 
+    parser.add_argument('-s', '--size', type=int, default='500', help='An integer for sample size')
+
+    parser.add_argument(
+        '-c', '--centers', type=int, default='3', help='An integer for number of blobs points sample centers'
+    )
+
     return parser
+
+
+def generate_blobs_points_sample(sample_size, blobs_centers_number):
+    X, y_true = make_blobs(n_samples=sample_size, centers=blobs_centers_number,
+                           cluster_std=.5, random_state=0)
+    X = X[:, ::-1]
+
+    return X
 
 
 def main():
     parser = get_args_parser()
     args = parser.parse_args()
+
+    blobs_points_sample = generate_blobs_points_sample(args.size, args.centers)
 
 
 if __name__ == "__main__":
