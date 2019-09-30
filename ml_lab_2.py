edcedd6490d3d326f955ad313675fbe910c09a6f
Max Pavlukhin <pavlukhinm@gmail.com>
2019-09-30 14:53:25 +0300

    Add argparse for ml_lab_2.py

--- ml_lab_2.py
+++ ml_lab_2.py
@@ -1,5 +1,17 @@
+import argparse
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(
+        description='ML Lab 2'
+    )
+
+    return parser
+
+
 def main():
-    pass
+    parser = get_args_parser()
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
